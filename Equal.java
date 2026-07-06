dbbc6c87fb8ea3d311262875165d82fe867bdffb
sujim72006-sudo <sujim72006@gmail.com>
2026-07-06 11:03:21 +0530

    Create Equal.java

---
+++ Equal.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    String str1 = "a";
+    String str2 = "a";
+    boolean result = str1.equals(str2);
+    System.out.println(result);
+  }
+}
