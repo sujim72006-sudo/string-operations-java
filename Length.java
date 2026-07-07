2867215de790b2fa0151cefb09112e52bd4b3ffa
sujim72006-sudo <sujim72006@gmail.com>
2026-07-07 09:17:22 +0530

    Create Length.java

---
+++ Length.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "suji";
+    System.out.println("String: " + name);
+    int length = name.length();
+    System.out.println("Length: " + length);
+  }
+}
